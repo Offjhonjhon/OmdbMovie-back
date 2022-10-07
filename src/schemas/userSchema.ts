3f92dc6694642b80a582e561f5dcf4d58963b996
João Victor Araujo <jjvictorac@gmail.com>
2022-10-07 11:34:23 -0300

    fix: password length requirement

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -3,12 +3,12 @@ import Joi from "joi";
 import { UserCreateData } from "../services/authService.js";
 
 export const signUpSchema = Joi.object<UserCreateData>({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    name: Joi.string().required()
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  name: Joi.string().required(),
 });
 
 export const signInSchema = Joi.object<UserCreateData>({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(10).required()
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
